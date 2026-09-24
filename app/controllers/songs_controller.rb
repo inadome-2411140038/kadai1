a208dc6cfba5c710cda0619916f1314cd88dbc79
2411140038f <2411140038f@ed.fuk.kindai.ac.jp>
2026-09-24 02:52:19 +0000

    validateとflashを追加

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,46 +4,58 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
+
   def new
     @song = Song.new
   end
+
 
   def create
     @song = Song.new(song_params)
 
     if @song.save
+      flash[:notice] = "曲を登録しました。"
       redirect_to @song
     else
+      flash.now[:alert] = "入力内容にエラーがあります。"
       render :new, status: :unprocessable_entity
     end
   end
+
 
   def show
     @song = Song.find(params[:id])
   end
 
+
   def edit
     @song = Song.find(params[:id])
   end
+
 
   def update
     @song = Song.find(params[:id])
 
     if @song.update(song_params)
+      flash[:notice] = "曲の情報を更新しました。"
       redirect_to @song
     else
+      flash.now[:alert] = "入力内容にエラーがあります。"
       render :edit, status: :unprocessable_entity
     end
   end
+
 
   def destroy
     @song = Song.find(params[:id])
     @song.destroy
 
+    flash[:notice] = "曲を削除しました。"
     redirect_to songs_path
   end
 
   private
+
 
   def song_params
     params.require(:song).permit(:title, :artist, :genre)
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,5 @@
 class Song < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :artist, presence: true
+  validates :genre, presence: true
 end
